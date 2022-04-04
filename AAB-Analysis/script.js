d1e8603e8d05b0c1997589ca18453508499bc117
Jago Van Dam <jagovandam@hotmail.com>
2022-04-05 09:02:26 +0930

    added cod by age group figure

--- AAB-Analysis/script.js
+++ AAB-Analysis/script.js
@@ -2,12 +2,27 @@ function setIframe() {
   let selected = document.getElementById("fig_select");
   let selectionID = selected.options[selected.selectedIndex].id;
   let selection = document.getElementById("fig_select").value;
-  document.getElementById("figure-display").src = selection
-  buttons = document.querySelector('#buttonBlock')
+  document.getElementById("figure-display").src = selection;
+  buttons = document.querySelector('#buttonBlock');
+  var subButtons = document.querySelectorAll("button.sub-button");
   switch(selectionID) {
     case 'figure2':
       buttons.style.display = 'flex';
-      let subButtons = document.querySelectorAll("button.sub-button");
+      subButtons[0].value = "Figures\\rates_seifa.html";
+      subButtons[0].innerHTML = "All deaths";
+      subButtons[1].value = "Figures\\rates_seifa_residents.html";
+      subButtons[1].innerHTML = "SA Residents";
+      subButtons.forEach(function(button) {
+        button.classList.remove('activeButton')
+      });
+      document.querySelector('button.sub-button').classList.add('activeButton');
+      break;
+    case 'figure4':
+      buttons.style.display = 'flex';
+      subButtons[0].value = "Figures\\cod_age_atsi.html";
+      subButtons[0].innerHTML = "Aboriginal CYP";
+      subButtons[1].value = "Figures\\cod_age.html";
+      subButtons[1].innerHTML = "Non-Aboriginal CYP";
       subButtons.forEach(function(button) {
         button.classList.remove('activeButton')
       });
